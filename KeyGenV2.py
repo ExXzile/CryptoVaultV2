8230249da2c99fc65056e22d21924ce9cf49c129
ExXzile <48256887+ExXzile@users.noreply.github.com>
2019-04-22 00:38:36 +0100

    Add files via upload

--- KeyGenV2.py
+++ KeyGenV2.py
@@ -1,14 +1,17 @@
+
+import string
+import secrets
 import random
 
-lower_keys = "abcdefghijklmnopqrstuwvxyz"
-upper_keys = "ABCDEFGHIJKLMNOPQRSTUWVXYZ"
-numbers = "1234567890"
-spec_chars = r'!"#$%&\'()*+,-./:;<=>?[]^_`|~'
+LOWER_KEYS = string.ascii_lowercase
+UPPER_KEYS = string.ascii_uppercase
+NUMBERS = "1234567890"
+SPEC_CHARS = r'!"#$%&\'()*+,-./:;<=>?[]^_`|~'
 
-soup_pool = [lower_keys,
-             upper_keys,
-             numbers,
-             spec_chars]
+soup_pot = [LOWER_KEYS,
+            UPPER_KEYS,
+            NUMBERS,
+            SPEC_CHARS]
 
 vectors = [2, 3, 3, 4]
 length_var = 4
@@ -18,16 +21,16 @@ random.shuffle(vectors)
 
 def _make_soup():
     soup = ''
-    for vector in vectors:
-        for n in range(vector):
-            soup += random.choice(soup_pool[n - 1])
+    for run, vector in enumerate(vectors):
+        for _ in range(vector):
+            soup += secrets.choice(soup_pot[run])
     return soup
 
 
 def _extra_soup(var):
     more_soup = ''
-    for n in range(random.randrange(var + 1)):
-        more_soup += random.choice(soup_pool[random.randrange(3)])
+    for _ in range(secrets.randbelow(var + 1)):
+        more_soup += secrets.choice(soup_pot[secrets.randbelow(len(vectors))])
     return more_soup
 
 
@@ -35,5 +38,3 @@ def key_gen():
     password = _make_soup() + _extra_soup(length_var)
     password = "".join(random.sample(password, len(password)))
     return password
-
-
